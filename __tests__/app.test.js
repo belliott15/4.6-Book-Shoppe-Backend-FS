9d6009f55b56a0a3886f1dad0ff97e652f30c4bb
Beau Elliott <belliott1516@gmail.com>
2022-06-13 17:46:01 -0700

    started to make book tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -7,9 +7,20 @@ describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+  it('GET book route returns all books in the data set', async () => {
+    const res = await request(app).get('/books');
+    const bookTitle = res.body.find((book) => book.id === '1');
+    expect(bookTitle).toHaveProperty('author', 'Kurt Vonnegut');
   });
+
+  it('GET book route returns all books in the data set', async () => {
+    const res = await request(app).get('/books/1');
+    const expected = {
+      
+    };
+    expect(res.body).toEqual(expected);
+  });
+
   afterAll(() => {
     pool.end();
   });
